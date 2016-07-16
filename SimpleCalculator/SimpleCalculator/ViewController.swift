72e7301c1f7372da2c032b420f45bce86b6ad54e
Taylor Frost <taylorhamblinfrost@gmail.com>
2016-07-16 16:20:09 -0600

    Got rid of the boiler plat code

---
+++ SimpleCalculator/SimpleCalculator/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Taylor Frost on 7/16/16.
+//  Copyright © 2016 Taylor Frost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+        
+    }
+
+}
+
