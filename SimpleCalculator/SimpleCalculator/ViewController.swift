6fb54e8a21e9a3421befc25bf3aeacb338f4e42d
Taylor Frost <taylorhamblinfrost@gmail.com>
2016-07-17 16:31:07 -0600

    Completed simple calculator, now on to Jareds

--- SimpleCalculator/SimpleCalculator/ViewController.swift
+++ SimpleCalculator/SimpleCalculator/ViewController.swift
@@ -9,15 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var isTypingNumber = false
+    
+    var firstNumber = 0
+    
+    var secondNumber = 0
+    
+    var operation = ""
 
+    @IBOutlet weak var calculatorDisplay: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+    }
+    
+
+    
+    @IBAction func numberTapped(sender: AnyObject) {
+        
+        let number = sender.currentTitle
+        
+        if isTypingNumber {
+            calculatorDisplay.text = calculatorDisplay.text! + String(number)
+        } else {
+            
+            calculatorDisplay.text  = number
+            
+            isTypingNumber = true
+            
+        }
+        
+    }
+    
+    @IBAction func calculationTapped(sender: AnyObject) {
+        
+        isTypingNumber = false
+        
+        firstNumber = calculatorDisplay.
         
         
-        
-        
+    }
+    
+    @IBAction func equalsTapped(sender: AnyObject) {
         
     }
 
 }
-
